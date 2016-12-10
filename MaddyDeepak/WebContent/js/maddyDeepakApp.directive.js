48c99df6556f7a605b39d5982f0d05d54175ce1c
maddydeepak <maddheshiya.deepak@gmail.com>
2016-12-10 13:45:02 +0530

    Maddy Deepak
    Maddy Deepak

---
+++ MaddyDeepak/WebContent/js/maddyDeepakApp.directive.js
@@ -0,0 +1,13 @@
+maddyDeepakApp.directive('headerView', function() {
+	return {
+		restrict: 'AE',
+		templateUrl: 'views/header.html'
+	}
+});
+
+maddyDeepakApp.directive('footerView', function() {
+	return {
+		restrict: 'AE',
+		templateUrl: 'views/footer.html'
+	}
+});
